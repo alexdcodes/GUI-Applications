1c7ac565202e83df5c4e4e8cbfd8aeeb3306c0d2
Alex Diker <alex.diker@live.ca>
2020-08-08 14:37:22 -0400

    Not Done
    Not Done

--- NoteEditor.py
+++ NoteEditor.py
@@ -7,4 +7,11 @@ w.Show()
 lB = wx.Button(w, label="Open",
                pos=(225, 5), size=(80,25))
 
-sB = 
+sB = wx.Button(w, label="Save",
+               pos=(315, 5), size=(80, 25))
+
+fn = wx.TextCtrl(w, pos=(5, 35), size=(210, 25))
+
+c = wx.TextCtrl(w, pos=(5, 35), size=(390, 260),
+                style=wx.TE_MULTILINE | wx.HSCROLL)
+a.MainLoop()
