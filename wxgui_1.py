02be27c87b3ba99e2acf0d398ab19c89dc033ca7
Alex Diker <alex.diker@live.ca>
2020-08-08 14:27:10 -0400

    Add files via upload

---
+++ wxgui_1.py
@@ -0,0 +1,15 @@
+import wx
+
+def xbc(event):
+    print ("I like you are learning")
+
+
+a = wx.App()
+
+w = wx.Frame(None, title="Scheduling Application",
+             size=(200, 100))
+button = wx.Button(w, label="Send Schedule")
+button.Bind(wx.EVT_BUTTON, xbc)
+
+w.Show()
+a.MainLoop()
